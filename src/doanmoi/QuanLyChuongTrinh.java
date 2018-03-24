f356054a5feb3c585f40950e43cccdb0aadd894c
TaiPtit <vuductaiptit@gmail.com>
2018-03-24 14:19:24 +0700

    udate directory

---
+++ src/doanmoi/QuanLyChuongTrinh.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doanmoi;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
+/**
+ *
+ * @author Dell
+ */
+public class QuanLyChuongTrinh extends JFrame{
+    
+   
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    private ChucNangChinhUI chucNangChinhUI;
+    
+    
+    
+    public QuanLyChuongTrinh(String tieude) {
+        super(tieude);
+        addControls();
+        addEvents();
+    }
+
+    
+    
+    private void addControls() {
+        Container con = getContentPane();
+        
+        chucNangChinhUI = new ChucNangChinhUI();
+        con.add(chucNangChinhUI);
+    }
+
+    private void addEvents() {
+    }
+
+   
+     public void showWindows() {
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    
+}
---
+++ src/doanmoi/QuanLyVanPhongPham.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doanmoi;
+
+/**
+ *
+ * @author Dell
+ */
+public class QuanLyVanPhongPham {
+    public static void main(String[] args) {
+        QuanLyChuongTrinh chuongTrinh = new QuanLyChuongTrinh("Quản lý văn phòng phẩm");
+        chuongTrinh.showWindows();
+    }
+    
+}
